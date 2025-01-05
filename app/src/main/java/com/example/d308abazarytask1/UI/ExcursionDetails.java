d9e36ae124eebe47b2bf133da7f429b6561ed400
aabazary <85041715+aabazary@users.noreply.github.com>
2025-01-04 22:03:48 -0800

    Added UI elements for Excursions/Vacations/Vacation details. Basic template based on video 1.

--- app/src/main/java/com/example/d308abazarytask1/UI/ExcursionDetails.java
+++ app/src/main/java/com/example/d308abazarytask1/UI/ExcursionDetails.java
@@ -1,9 +1,6 @@
 package com.example.d308abazarytask1.UI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,22 +10,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.d308abazarytask1.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ExcursionDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this,VacationList.class);
-                intent.putExtra("test","Information Sent");
-                startActivity(intent);
-            }
-        }));
+        setContentView(R.layout.activity_excursion_details);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/d308abazarytask1/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308abazarytask1/UI/VacationDetails.java
@@ -3,7 +3,6 @@ package com.example.d308abazarytask1.UI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,23 +11,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.d308abazarytask1.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class VacationDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener((new View.OnClickListener() {
+        setContentView(R.layout.activity_vacation_details);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
+        fab.setOnClickListener(new View.OnClickListener(){
+
             @Override
-            public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this,VacationList.class);
-                intent.putExtra("test","Information Sent");
+            public void onClick(View v) {
+                Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
                 startActivity(intent);
             }
-        }));
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
