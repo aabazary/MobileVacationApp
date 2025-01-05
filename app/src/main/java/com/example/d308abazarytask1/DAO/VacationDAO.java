3ebd071ac0e4c75e76896c8b7a914a49f290b67d
aabazary <85041715+aabazary@users.noreply.github.com>
2025-01-04 16:43:22 -0800

    Created Database and DAO for Excursion and Vacations

---
+++ app/src/main/java/com/example/d308abazarytask1/DAO/VacationDAO.java
@@ -0,0 +1,28 @@
+package com.example.d308abazarytask1.DAO;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.d308abazarytask1.Entities.Excursion;
+import com.example.d308abazarytask1.Entities.Vacation;
+
+import java.util.List;
+
+@Dao
+public interface VacationDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Vacation vacation);
+
+    @Update
+    void update(Vacation vacation);
+
+    @Delete
+    void delete(Vacation vacation);
+
+    @Query("SELECT * FROM VACATIONS ORDER BY id ASC")
+    List<Excursion> getAllVacations();
+}
